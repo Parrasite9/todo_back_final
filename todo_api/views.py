c3c3b6b2ef547401ed707d4bcd18580e8b363524
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2023-01-22 20:44:44 -0600

    new repo

---
+++ todo_api/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .serializer import TodoSerializer
+from .models import Todo
+
+# Create your views here.
+class TodoList(generics.ListCreateAPIView):
+    queryset = Todo.objects.all().order_by('id')
+    serializer_class = TodoSerializer
+
+class TodoDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Todo.objects.all().order_by('id')
+    serializer_class = TodoSerializer
